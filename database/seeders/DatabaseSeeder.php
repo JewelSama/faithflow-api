928907099e338e51c197fe698cf76bfa642a4299
JewelSama <osafilejewel@gmail.com>
2025-06-03 23:02:48 +0100

    add DepartmentSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ParishSeeder::class,
             UserSeeder::class,
             MemberSeeder::class,
+            DepartmentSeeder::class,
         ]);
     }
 }
