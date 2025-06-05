6baf63cd59eb05ef2fc5e24f7844082de60b34a0
JewelSama <osafilejewel@gmail.com>
2025-06-06 00:03:39 +0100

    add(routes): implement routes for tithes, offerings, and donations

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@ use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\PrayerRequestController;
 use App\Http\Controllers\TestimonyController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\TitheController;
+use App\Http\Controllers\OfferingController;
+use App\Http\Controllers\DonationController;
 
 
 
@@ -35,6 +38,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('parishes/prayer-requests/{id}', [ParishController::class, 'prayerRequests']);
     Route::get('parishes/testimonies/{id}', [ParishController::class, 'testimonies']);
     Route::get('parishes/events/{id}', [ParishController::class, 'events']);
+    Route::get('parishes/announcements/{id}', [ParishController::class, 'announcements']);
+    Route::get('parishes/offerings/{id}', [ParishController::class, 'offerings']);
+    Route::get('parishes/tithes/{id}', [ParishController::class, 'tithes']);
+    Route::get('parishes/donations/{id}', [ParishController::class, 'donations']);
+    Route::get('parishes/finances/{id}', [ParishController::class, 'finances']);
     Route::get('parishes/{id}', [ParishController::class, 'show']);
     Route::put('parishes/{id}', [ParishController::class, 'update']);
     Route::delete('parishes/{id}', [ParishController::class, 'destroy']);
@@ -76,4 +84,31 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('events', [EventController::class, 'store']);
     Route::get('events/{id}', [EventController::class, 'show']);
     Route::delete('events/{id}', [EventController::class, 'destroy']);
+
+    Route::get('announcements', [AnnouncementController::class, 'index']);
+    Route::post('announcements', [AnnouncementController::class, 'store']);
+    Route::get('announcements/{id}', [AnnouncementController::class, 'show']);
+    Route::put('announcements/{id}', [AnnouncementController::class, 'update']);
+    Route::delete('announcements/{id}', [AnnouncementController::class, 'destroy']); 
+
+    Route::prefix('tithes')->group(function () {
+        Route::get('/', [TitheController::class, 'index']);
+        Route::post('/', [TitheController::class, 'store']);
+        Route::get('{id}', [TitheController::class, 'show']);
+        Route::delete('{id}', [TitheController::class, 'destroy']);
+    });
+    
+    Route::prefix('offerings')->group(function () {
+        Route::get('/', [OfferingController::class, 'index']);
+        Route::post('/', [OfferingController::class, 'store']);
+        Route::get('{id}', [OfferingController::class, 'show']);
+        Route::delete('{id}', [OfferingController::class, 'destroy']);
+    });
+    
+    Route::prefix('donations')->group(function () {
+        Route::get('/', [DonationController::class, 'index']);
+        Route::post('/', [DonationController::class, 'store']);
+        Route::get('{id}', [DonationController::class, 'show']);
+        Route::delete('{id}', [DonationController::class, 'destroy']);
+    });
 });
